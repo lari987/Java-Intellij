27a659c1c5bb22d90bab998e1154b4adb095f60e
lari987 <michelazzolarissa272@gmail.com>
2022-07-14 17:36:41 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args){
+        Gato gato = new Gato(null,null,null);
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 10;
+        System.out.println("Hello World! " + (a + b));*/
+    }
+}
